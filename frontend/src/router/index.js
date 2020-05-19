e2bc4aaf00af34f350ab610fee761e3ce8521320
Euan Gordon <ejg9@hw.ac.uk>
2020-05-19 16:31:22 +0100

    patch

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Home from "../views/home";
+import Home from "../views/home.vue";
 
 Vue.use(VueRouter);
 
@@ -9,14 +9,14 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
-  }
+    component: Home,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
