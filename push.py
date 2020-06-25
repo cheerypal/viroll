25665aec2e69bd1ed607a078aa13ba901d03456f
Euan Gordon <ejg9@hw.ac.uk>
2020-06-25 16:43:05 +0100

    build12

--- push.py
+++ push.py
@@ -7,19 +7,21 @@ def main ():
 
     lint = False
     heroku = False
+    commit = False
 
     for i in range(len(sys.argv)):
+
         if sys.argv[i] == "-l" or sys.argv[i] == "lint" :
             lint = True
             
         if sys.argv[i] == "-h" or sys.argv[i] == "heroku":
             heroku = True
 
+    
     commitMsg = input("Type your commit msg here:\n")
 
     if lint == True or heroku == True:
-        os.system("cd frontend")
-        
+        os.chdir(os.path.abspath("C:/Users/euang/Documents/viroll/frontend"))
         if lint == True:
            cp = cmd.run("npm run lint", check=True, shell=True)
            print(cp)
@@ -27,14 +29,15 @@ def main ():
            cp = cmd.run('npm run build', check=True, shell=True)
            print(cp)
 
-    cp = cmd.run("git add .", check=True, shell=True)
-    print(cp)
-    cp = cmd.run(f"git commit -m '{commitMsg}'", check=True, shell=True)
-    print(cp)
-    cp = cmd.run("git push", check=True, shell=True)
-    print(cp)
+        cp = cmd.run("git add .", check=True, shell=True)
+        print(cp)
+        cp = cmd.run(f"git commit -m '{commitMsg}'", check=True, shell=True)
+        print(cp)
+        cp = cmd.run("git push", check=True, shell=True)
+        print(cp)
 
     if heroku == True:
+        os.chdir(os.path.abspath("C:/Users/euang/Documents/viroll"))
         cp = cmd.run("git subtree --prefix frontend push heroku master" , check=True, shell=True)
         print(cp)
 
