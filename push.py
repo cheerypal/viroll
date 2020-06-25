24121851192ba656dcfc4f23ba8dc79a5939ad1c
Euan Gordon <ejg9@hw.ac.uk>
2020-06-25 16:15:44 +0100

    'ddd'

---
+++ push.py
@@ -0,0 +1,47 @@
+import subprocess as cmd
+import sys, os
+
+#initialise on powershell
+
+def main ():
+
+    lint = False
+    heroku = False
+
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == "-l" or sys.argv[i] == "lint" :
+            lint = True
+            
+        if sys.argv[i] == "-h" or sys.argv[i] == "heroku":
+            heroku = True
+
+    commitMsg = input("Type your commit msg here:\n")
+    
+    
+
+    if lint == True or heroku == True:
+        cp = cmd.run("cd frontend", check=True, shell=True)
+        print(cp)
+        if lint == True:
+           cp = cmd.run("npm run lint", check=True, shell=True)
+           print(cp)
+        if heroku == True:
+           cp = cmd.run('npm run build', check=True, shell=True)
+           print(cp)
+
+    print("ssssss")
+    cp = cmd.run("git add .", check=True, shell=True)
+    print(cp)
+    cp = cmd.run(f"git commit -m '{commitMsg}'", check=True, shell=True)
+    print(cp)
+    cp = cmd.run("git push", check=True, shell=True)
+    print(cp)
+    print("sss")
+
+    if heroku == True:
+        cp = cmd.run("git subtree --prefix frontend push heroku master" , check=True, shell=True)
+        print(cp)
+
+
+if __name__ == '__main__':
+    main()
